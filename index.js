1818fd70df890579861888746f3fa208c23ed2d1
MariySemenenko <mashyni92@gmail.com>
2023-08-04 18:13:29 +0300

    alt hreeft f

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
 const contacts = require("./contacts.js");
 
-const { Command } = require('commander');
+const { Command } = require("commander");
 const program = new Command();
 program
-  .option('-a, --action <type>', 'choose action')
-  .option('-i, --id <type>', 'user id')
-  .option('-n, --name <type>', 'user name')
-  .option('-e, --email <type>', 'user email')
-  .option('-p, --phone <type>', 'user phone');
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
 program.parse(process.argv);
 
 const argv = program.opts();
 
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+      break;
 
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.table(oneContact);
+      break;
 
-const invokeAction = async ({action, id, name, email, phone}) => {
-    switch (action) {
-        case 'list':
-            const allContacts = await contacts.listContacts();
-            return console.table(allContacts);
-            break;
+    case "add":
+      const newcontacts = await contacts.addContact(name, email, phone);
+      return console.table(newcontacts);
+      break;
 
-            case 'get':
-                const oneContact = await contacts.getContactById(id);
-                return console.table(oneContact);
-                break;
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      return console.table(deleteContact);
+      break;
 
-                case 'add':
-                    const newcontacts = await contacts.addContact(name, email, phone);
-                    return console.table(newcontacts);
-                    break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
 
-                    case 'remove' :
-                        const deleteContact = await contacts.removeContact(id);
-                        return console.table(deleteContact);
-                        break;
-
-                        default:
-                console.warn('\x1B[31m Unknown action type!');
-
-    }
-} 
-  
 invokeAction(argv);
-
-
-
 
 //invokeAction({action: 'read'});
 //invokeAction({action: 'getById', id: 'Z5sbDlS7pCzNsnAHLtDJd'});
