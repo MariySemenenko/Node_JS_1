b3030e04e056bb0aae67e006384d903ab19d53e1
MariySemenenko <mashyni92@gmail.com>
2023-08-04 21:13:10 +0300

    n6

--- contacts.js
+++ contacts.js
@@ -34,7 +34,7 @@ const removeContact = async (contactId) => {
   if (index === -1) {
     return null;
   }
-  const { result } = contacts.splice(index, 1);
+  const  [result]  = contacts.splice(index, 1);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return result;
 };
